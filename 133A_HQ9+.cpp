7a7e29ea4ce352b367f55b85133057296bd62bb5
ankitsharma07 <ankitsharma0711@gmail.com>
2018-10-10 12:52:00 +0530

    546

--- 133A_HQ9+.cpp
+++ 133A_HQ9+.cpp
@@ -7,8 +7,6 @@ int main(int argc, char const *argv[]){
 	string p;
 	cin >> p;
 
-	transform(p.begin(), p.end(), p.begin(), ::toupper);
-
 	for (int i=0; i<p.length(); i++){
 		if(p[i] == 'H' || p[i] == 'Q' || p[i] == '9'){
 			cout << "YES\n";
